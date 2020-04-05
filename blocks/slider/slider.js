667e2d932d4e0a4c92c09ee02d8f83d6f373ac9f
alexandrprokhorov1988 <alexandrprokhorov1988@gmail.com>
2020-04-05 20:29:32 +0300

    slide js change

--- blocks/slider/slider.js
+++ blocks/slider/slider.js
@@ -1,22 +1,15 @@
 document.addEventListener('click', function () {
     moveSlider();
 });
+let left = 0;
 
 function moveSlider() {
-    let step = 0;
-    let img = document.querySelectorAll('.slider__img');
-    let link = document.querySelectorAll('.slider__link');
-    console.log(img);
-    let image = document.createElement('img');
-    image.classList.add("slider__img");
-    image.src = img[step].src;
-    link[step].remove();
-    img[step].remove();
-    let a = document.createElement("a");
-    a.classList.add("slider__link");
-    a.href = "#";
-    a.appendChild(image);
-    document.querySelector(".slider__container").appendChild(a);
+    let container = document.querySelector(".slider__container");
+    left = left - 260;
+    if (left < -1560) {
+        left = 0;
+    }
+    container.style.left = left + 'px';
 }
 
 setInterval(moveSlider, 3000);
